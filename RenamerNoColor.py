a17cfbe935b96e9a089fdb7fa4fbd4defa8c6892
Tinker <147129931+Tinkering-Around@users.noreply.github.com>
2024-01-17 22:04:14 -0700

    New Function
    Added a new function where you can choose everything or just image formats. 
    
    Image formats are: .jpg", .jpeg, .png, .gif, .bmp

--- RenamerNoColor.py
+++ RenamerNoColor.py
@@ -27,13 +27,22 @@ def list_files():
 
 def modify_category(extensions_count):
     print("\nSelect a category to modify:")
+    print("0. All formats")
     for index, (ext, _) in enumerate(extensions_count.items(), start=1):
         print("{}. {}".format(index, ext))
+    print("{}. All picture formats".format(len(extensions_count) + 1))
 
     try:
         choice = int(input("Enter the number of the category: "))
-        selected_category = list(extensions_count.keys())[choice - 1]
-        print("\nModifying category: {}".format(selected_category))
+        if choice == 0:
+            selected_category = "all"
+            print("\nModifying category: {}".format(selected_category))
+        elif choice <= len(extensions_count):
+            selected_category = list(extensions_count.keys())[choice - 1]
+            print("\nModifying category: {}".format(selected_category))
+        else:
+            selected_category = "pictures"
+            print("\nModifying category: {}".format(selected_category))
 
         avoid_file = input("\nEnter the name of the file to avoid or press Enter to continue: ")
         avoid_file = avoid_file.lower()
@@ -44,7 +53,7 @@ def modify_category(extensions_count):
             if os.path.isfile(file) and file.lower() != avoid_file:
                 _, extension = os.path.splitext(file)
                 extension = extension.lower()
-                if extension == selected_category:
+                if selected_category == "all" or (selected_category == "pictures" and extension in {".jpg", ".jpeg", ".png", ".gif", ".bmp"}):
                     new_name = "{}{}".format(count, extension)
                     os.rename(file, new_name)
                     print("| {:<30} | {:<30} |".format(file, new_name))
